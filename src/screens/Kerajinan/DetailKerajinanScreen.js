8f6a9e8bd6814ea108c571f654079193fa7840b6
mayonice1424 <friyandrailham14@gmail.com>
2022-07-07 14:41:18 +0700

    Menambahkan halaman artikel dan kerajinan

---
+++ src/screens/Kerajinan/DetailKerajinanScreen.js
@@ -0,0 +1,24 @@
+
+import React,{useState} from 'react';
+import {View, Text,Image, SafeAreaView,TouchableOpacity, ScrollView} from 'react-native';
+import colorStyle from '../../styles/colorStyle';
+import textStyle from '../../styles/textStyle';
+import layoutStyle from '../../styles/layoutStyle';
+import kerajinanStyle from '../../styles/kerajinanStyle';
+import Kerajinan from '../routes/KerajinanRoutes';
+import cardStyle from '../../styles/cardStyle';
+const KerajinanScreen = ({navigation}) => {
+    return(
+    <View style={kerajinanStyle.container}>
+        <View style={kerajinanStyle.content}>
+            <ScrollView>
+                 <View>
+                    
+                 </View>
+            </ScrollView>
+        </View>
+   </View>
+   )
+};
+
+export default KerajinanScreen;
