1d7b6ead77eca9bd2eaafd7e79b3228761bd25a8
mayonice1424 <friyandrailham14@gmail.com>
2022-07-09 13:21:27 +0700

    menambahkan splash screen

---
+++ src/styles/splashScreenStyle.js
@@ -0,0 +1,24 @@
+import {View, Text, StyleSheet, Colors} from 'react-native';
+import textStyle from './textStyle';
+import colorStyle from './colorStyle';
+
+const splashScreenStyle = StyleSheet.create({
+    container: {
+        flex: 1,
+        // alignItems:"center",
+        height: '100%',
+        backgroundColor: '#FCFFFC',
+        padding: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignContent: 'center',
+    },
+    content: {
+        paddingBottom:80,
+        marginLeft: '4%',
+        marginRight: '4%',
+        height: '100%',
+      },
+});
+
+export default splashScreenStyle;
